5e6c7e9a3355f13a900a9f7773da2e7fff5fb728
Devlusket <tsuunakage@gmail.com>
2026-03-16 15:30:08 -0300

    add: UsuarioService

--- src/app/core/services/produto.ts
+++ src/app/core/services/produto.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProdutoRequest, ProdutoResponse } from '../models/produto.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Produto {
+export class ProdutoService {
+
   
+  private readonly apiUrl = `${environment.apiUrl}/produtos`;
+
+  private http = inject(HttpClient);
+
+
+  listar(): Observable<ProdutoResponse[]> {
+    return this.http.get<ProdutoResponse[]>(this.apiUrl);
+  }
+
+  buscarPorId(id: number):  Observable<ProdutoResponse> {
+    return this.http.get<ProdutoResponse>(`${this.apiUrl}/${id}`);
+  }
+
+  criar(request: ProdutoRequest): Observable<ProdutoResponse> {
+    return this.http.post<ProdutoResponse>(this.apiUrl, request);
+  }
+
+  atualizar(id: number, request: ProdutoRequest): Observable<ProdutoResponse> {
+    return this.http.put<ProdutoResponse>(`${this.apiUrl}/${id}`, request);
+  }
+
+  desativar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+
+
+
 }
