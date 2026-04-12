479e825fc0831a417c3e32a2bda5047f3a79da6b
Devlusket <tsuunakage@gmail.com>
2026-04-12 17:36:09 -0300

    change: environment to vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://controle-de-estoque-backend-7h07.onrender.com/'
+  apiUrl: 'https://controle-de-estoque-backend-7h07.onrender.com'
 };
